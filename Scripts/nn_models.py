01b098a7e4ad9fedb7ae8dcc738208562b0159f5
Kristina Popova <kpopova@funkenzutzler.isds.tugraz.at>
2022-01-11 23:30:21 +0000

    baseline models

--- Scripts/nn_models.py
+++ Scripts/nn_models.py
@@ -3,6 +3,42 @@ from keras.layers import Concatenate, LSTM, TimeDistributed, Dense, BatchNormali
 import tensorflow.compat.v1 as tf
 
 
+def baseline_model(max_len, n_words, n_tags):
+    """
+    :param max_len: Max. submission length
+    :param n_words: Number of words in the vocabulary
+    :param n_tags: Number of classes
+    :return: Keras model
+
+    The model is built with a set of tokenized sentences
+    of length 'max_len'.
+    The Embedding layer creates features out of the tokens.
+    """
+    input = Input(shape=(max_len,))
+
+    model = Embedding(input_dim=n_words, output_dim=max_len, input_length=max_len)(input)
+    model = Dropout(0.1)(model)
+    model = Bidirectional(LSTM(units=150, return_sequences=True, recurrent_dropout=0.3))(model)
+
+    out = Dense(n_tags, activation="softmax")(model)
+    return Model(input, out)
+
+
+def baseline_additional_features(max_len, n_words, n_tags, num_feats):
+    input_tokens = Input(shape=(max_len,))
+    emb = Embedding(input_dim=n_words, output_dim=300, input_length=max_len)(input_tokens)
+
+    input_feats = Input(shape=(max_len, num_feats,))
+    fts = Dense(n_tags, activation='softmax')(input_feats)
+
+    model = Concatenate()([emb, fts])
+    model = SpatialDropout1D(0.1)(model)
+    model = Bidirectional(LSTM(units=200, return_sequences=True, recurrent_dropout=0.2))(model)  # variational biLSTM
+
+    out = TimeDistributed(Dense(n_tags, activation="softmax"))(model)
+    return Model([input_tokens, input_feats], out)
+
+
 def ElmoEmbedding(x):
     return elmo_model(inputs={"tokens": tf.squeeze(tf.cast(x, tf.string)),
                               "sequence_len": tf.constant(batch_size*[max_len])},
