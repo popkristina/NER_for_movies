8070d203c3312ff0964237a29367dfb7e2566332
popkristina <pop.kristina@gmail.com>
2023-04-11 21:52:33 +0200

    main pipeline

---
+++ pipline.py
@@ -0,0 +1,40 @@
+# Imports
+import pandas as pd
+import numpy as np
+import os
+import json
+
+# import tensorflow.compat.v1 as tf
+# import tensorflow_hub as hub
+# from tensorflow.keras.utils import to_categorical, plot_model
+# from tensorflow.compat.v1.keras import backend as K
+
+# from keras.models import Model, Input
+# from keras.layers import Concatenate, LSTM, TimeDistributed, Dense, \
+#     BatchNormalization, Bidirectional, Lambda
+
+from Scripts.preprocess import *
+from Scripts.feature_extraction import *
+from Scripts.plotting_functions import *
+# from Scripts.data_manipulation import *
+from Scripts.evaluate import *
+# from Scripts.train_models import *
+from Scripts.assemble import *
+
+# Insert text(s)
+# TODO: Fix input type
+texts = []
+
+
+# Preprocess text(s)
+
+
+# Extract features from text(s)
+
+
+# Annotate text(s)
+
+
+# Assemble predictions from text(s)
+
+
