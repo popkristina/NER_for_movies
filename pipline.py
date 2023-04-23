674d1987d02d0f1e99235a9f2df43414ad8d4ec2
popkristina <pop.kristina@gmail.com>
2023-04-23 22:21:25 +0200

    update inference pipeline

--- pipline.py
+++ pipline.py
@@ -5,18 +5,45 @@ import os
 import json
 
 import torch
-
+from transformers import BertTokenizer, RobertaTokenizer
 # import tensorflow.compat.v1 as tf
 # import tensorflow_hub as hub
 # from tensorflow.keras.utils import to_categorical, plot_model
 # from tensorflow.compat.v1.keras import backend as K
 
 from Scripts.preprocess import *
-from transformers import BertConfig, BertModel
-# from Scripts.data_manipulation import *
+from transformers import BertForTokenClassification, RobertaForTokenClassification
+from Scripts.data_manipulation import *
 from Scripts.evaluate import *
-# from Scripts.train_models import *
 from Scripts.assemble import *
+
+
+def generate_predictions(grouped_submissions, model, tokenizer):
+    all_predictions_list = []
+
+    for sub in grouped_submissions:
+        tokenized_submission = tokenizer.encode(sub)
+        input_ids = torch.tensor([tokenized_submission])
+        input_masks = torch.tensor([[float(i != 0.0) for i in ii] for ii in input_ids])
+        with torch.no_grad():
+            output = model(input_ids, attention_mask=input_masks)
+        label_indices = np.argmax(output[0].to('cpu').numpy(), axis=2)
+        label_indices = label_indices[0]
+        label_indices = label_indices[1:-1]
+
+        tokens = tokenizer_roberta.convert_ids_to_tokens(input_ids.to('cpu').numpy()[0])
+        new_tokens, new_labels = [], []
+        for token, label_idx in zip(tokens, label_indices):
+            if token.startswith("##"):
+                new_tokens[-1] = new_tokens[-1] + token[2:]
+            else:
+                new_labels.append(tag_values[label_idx])
+                new_tokens.append(token)
+
+        all_predictions_list.append(new_labels)
+
+    return all_predictions_list
+
 
 # Insert text(s)
 # TODO: Fix input type
@@ -43,7 +70,26 @@ texts['text'] = texts['title'] + " " + texts['selftext']
 texts = texts[['id', 'text']]
 texts['text'] = texts['text'].apply(replace)
 
-# Tokenize text
+tag_values = ['B-actor-pos',
+              'B-movie-neg',
+              'B-gen-neg',
+              'I-keyword-neg',
+              'O', 'I-gen-neg',
+              'I-actor-pos',
+              'B-keyword-pos',
+              'I-actor-neg',
+              'B-gen-pos',
+              'B-keyword-neg',
+              'B-actor-neg',
+              'I-movie-pos',
+              'I-gen-pos',
+              'I-keyword-pos',
+              'B-movie-pos',
+              'I-movie-neg',
+              'PAD']
+
+
+# Tokenize text (for ELMO)
 # TODO: Speed up
 tokenized_texts = pd.DataFrame()
 for text, id in zip(texts.text, texts.id):
@@ -53,10 +99,39 @@ for text, id in zip(texts.text, texts.id):
 # Extract features from text(s)
 # TODO: Removed from inference pipeline -> results unsatisfactory
 
+#################################################################################################
 # Load models
-bert_large_cased = torch.load("models/bert_base_multilingual_cased", map_location=torch.device('cpu'))
-bert_base_mult = torch.load("models/bert_base_multilingual_cased", map_location=torch.device('cpu'))
-roberta = torch.load("models/roberta", map_location=torch.device('cpu'))
+bert_large_cased = BertForTokenClassification.from_pretrained("models/bert-large-cased/")
+bert_base_mult = BertForTokenClassification.from_pretrained("models/bert-base-multilingual-cased/")
+roberta = RobertaForTokenClassification.from_pretrained("models/roberta-large/")
+
+# Load BERT tokenizers
+tokenizer_bert_large = BertTokenizer.from_pretrained('bert-large-cased', do_lower_case=False)
+tokenizer_bert_mult = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+tokenizer_roberta = RobertaTokenizer.from_pretrained('roberta-large', do_lower_case=False)
+
+# Prepare submissions for inference
+grouped = group_sents_nolabels(tokenized_texts)
+grouped_submissions = [[word[1] for word in sub] for sub in grouped]
+
+# Predictions generation
+predictions_roberta = generate_predictions(
+    grouped_submissions, roberta, tokenizer_roberta)
+_, assembled_predictions_roberta = assemble_predictions(
+    predictions_roberta, grouped_submissions, grouped, 300)
+
+predictions_bert_large_cased = generate_predictions(
+    grouped_submissions, bert_large_cased, tokenizer_bert_large)
+_, assembled_predictions_bert_lg_cased = assemble_predictions(
+    predictions_bert_large_cased, grouped_submissions, grouped, 300)
+
+predictions_bert_mult = generate_predictions(
+    grouped_submissions, bert_base_mult, tokenizer_bert_mult)
+_, assembled_predictions_bert_mult = assemble_predictions(
+    predictions_bert_mult, grouped_submissions, grouped, 300
+)
+
+# Bert large cased labels
 
 # Annotate text(s)
 
