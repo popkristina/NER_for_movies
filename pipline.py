17bd1ca286f7b0ca8800e2c8d2441be8e00157e8
popkristina <pop.kristina@gmail.com>
2023-04-24 21:01:32 +0200

    pipeline update

--- pipline.py
+++ pipline.py
@@ -1,22 +1,23 @@
 # Imports
 import pandas as pd
 import numpy as np
-import os
 import json
 
 import torch
-from transformers import BertTokenizer, RobertaTokenizer
-# import tensorflow.compat.v1 as tf
-#import tensorflow as tf
-# import tensorflow_hub as hub
-# from tensorflow.keras.utils import to_categorical, plot_model
-# from tensorflow.compat.v1.keras import backend as K
+from transformers import BertTokenizer, BertForTokenClassification
+from transformers import RobertaTokenizer, RobertaForTokenClassification
 
 from Scripts.preprocess import *
-from transformers import BertForTokenClassification, RobertaForTokenClassification
 from Scripts.data_manipulation import *
-from Scripts.evaluate import *
 from Scripts.assemble import *
+
+
+# FUNCTIONS AND PARAMETERS  ##################################################################
+
+max_len = 300
+batch_size = 32
+with open("helper_dicts/i2tg.json") as idx2tag_save:
+    idx2tag = json.load(idx2tag_save)
 
 
 def generate_predictions(grouped_submissions, model, tokenizer):
@@ -45,14 +46,31 @@ def generate_predictions(grouped_submissions, model, tokenizer):
 
 
 def generate_predictions_1(grouped_submissions, model):
-    all_predictions_list = []
+    orig_len = len(grouped_submissions)
+    while len(grouped_submissions) % 32 != 0:
+        grouped_submissions.append(grouped_submissions[-1])
+
+    padded_grouped = pad_textual_data(grouped_submissions, max_len)
+    preds = model.predict(np.array(padded_grouped))
+    p = np.argmax(preds, axis=-1)
+
+    predictions = from_num_to_class(p, idx2tag)[:orig_len]
+    padded_grouped = padded_grouped[:orig_len]
+    return predictions, padded_grouped
+
+
+def ElmoEmbedding(x):
+    return elmo_model(inputs={"tokens": tf.squeeze(tf.cast(x, tf.string)),
+                              "sequence_len": tf.constant(batch_size*[max_len])},
+                      signature="tokens",
+                      as_dict=True)["elmo"]
 
 
 
-    return all_predictions_list
 
 # Insert text(s)
 # TODO: Fix input type
+
 
 """texts_1 = pd.read_csv(
     "data/different_formats/MovieSuggestions_submissions_pretty.txt",
@@ -116,15 +134,6 @@ tokenizer_bert_large = BertTokenizer.from_pretrained('bert-large-cased', do_lowe
 tokenizer_bert_mult = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
 tokenizer_roberta = RobertaTokenizer.from_pretrained('roberta-large', do_lower_case=False)
 
-# Load ELMO model
-json_file = open("models/elmo_best.json", "r")
-loaded_model_json = json_file.read()
-json_file.close()
-#bilstm_elmo = tf.keras.models.model_from_json(loaded_model_json)
-#model.compile(optimizer=param_dict["optimizer"], loss=param_dict["loss"], metrics=param_dict["metrics"])
-#model.summary()
-#model.load_weights("models/" + model_name + ".h5")
-
 # Prepare submissions for inference
 grouped = group_sents_nolabels(tokenized_texts)
 grouped_submissions = [[word[1] for word in sub] for sub in grouped]
@@ -133,20 +142,45 @@ grouped_submissions = [[word[1] for word in sub] for sub in grouped]
 predictions_roberta = generate_predictions(
     grouped_submissions, roberta, tokenizer_roberta)
 _, assembled_predictions_roberta = assemble_predictions(
-    predictions_roberta, grouped_submissions, grouped)
+    predictions_roberta, grouped_submissions, grouped, 'roberta')
 
-predictions_bert_large_cased = generate_predictions(
+"""predictions_bert_large_cased = generate_predictions(
     grouped_submissions, bert_large_cased, tokenizer_bert_large)
 _, assembled_predictions_bert_lg_cased = assemble_predictions(
-    predictions_bert_large_cased, grouped_submissions, grouped)
+    predictions_bert_large_cased, grouped_submissions, grouped)"""
 
 predictions_bert_mult = generate_predictions(
     grouped_submissions, bert_base_mult, tokenizer_bert_mult)
 _, assembled_predictions_bert_mult = assemble_predictions(
-    predictions_bert_mult, grouped_submissions, grouped)
+    predictions_bert_mult, grouped_submissions, grouped, 'bert_mult')
 
+import tensorflow as tf
+import tensorflow.compat.v1 as tf
+import tensorflow_hub as hub
+from tensorflow.compat.v1.keras import backend as K
+
+# Load ELMO model
+#tf.disable_eager_execution()
+tf.compat.v1.disable_eager_execution()
+sess = tf.compat.v1.Session()
+K.set_session(sess)
+elmo_model = hub.Module("models/elmo_3/", trainable=True) # "https://tfhub.dev/google/elmo/3"
+sess.run(tf.global_variables_initializer())
+sess.run(tf.tables_initializer())
+
+json_file = open("models/elmo.json", "r")
+loaded_model_json = json_file.read()
+json_file.close()
+bilstm_elmo = tf.keras.models.model_from_json(loaded_model_json)
+bilstm_elmo.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+bilstm_elmo.load_weights("models/elmo.h5")
+
+elmo_predictions, grouped_submissions = \
+    generate_predictions_1(grouped_submissions, bilstm_elmo)
+_, assembled_predictions_elmo = assemble_predictions(
+    elmo_predictions, grouped_submissions, grouped, 'elmo')
 
 # Assemble predictions from text(s)
-ensemble_predictions = ensemble_appr(assembled_predictions_roberta, assembled_predictions_bert_lg_cased, assembled_predictions_bert_mult, [])
+ensemble_predictions = ensemble_appr(
+    assembled_predictions_roberta, assembled_predictions_bert_mult, assembled_predictions_elmo)
 print(ensemble_predictions)
-
